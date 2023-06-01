762becb4fdd50e80b4e65b084b04eb05b6589ffe
Sakthivel Nadar <55133980+SakthivelNadar@users.noreply.github.com>
2023-06-01 16:28:42 +0530

    releases: adapt for yogurt.

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="SakthivelNadar"
+export GITHUB_EMAIL="s2234nadar@gmail.com"
 
-export device=""
+export device="yogurt"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="LineageOS"
+export ROM_DIR="${WORKSPACE}/../LineageOS-13"
+export ROM_VERSION="13"
+export local_manifest_url="https://raw.githubusercontent.com/yogurt-devs/local_manifests/main/thirteen.xml"
+export manifest_url="https://github.com/LineageOS"
+export rom_vendor_name="lineage"
+export branch="lineage-20.0"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="clean"
+export generate_incremental="false"
+export upload_recovery="true"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="yogurt-devs/releases"
 
 export timezone="UTC"
